932621a2290ae2dca6c491b6b15114615586c62d
Rai Phairow <rai.phairow@pubnub.com>
2021-03-18 12:56:05 -0700

    got http response!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+extern crate hyper;
+
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::time::{sleep, Duration};
+use hyper::{Client};
 
 type Db = Arc<Mutex<HashMap<String, String>>>;
 
@@ -71,6 +74,41 @@ async fn fetch_greed(db: Db) {
 
 async fn fetch_whale(db: Db) {
     println!("-fetch_whale-");
+
+    let client = Client::new();
+
+    // let https = HttpsConnector::new();
+    // let client = Client::builder().build::<_, hyper::Body>(https);
+    
+
+    let uri = "http://api.whale-alert.io/v1/transactions?limit=100&start=1616085702&min_value=500000&api_key=aZgs2HiCOeTkvMDGn1ltbvp1IysxRiQF".parse();
+    let parsed_uri;
+
+    match uri {
+        Err(why) => {
+            println!("fetch_whale::URI-ERROR {}", why);
+            return;
+        },
+        Ok(value) => {
+            parsed_uri = value;
+        }
+    }
+
+    let resp = client.get(parsed_uri).await;
+
+    let status;
+
+    match resp {
+        Err(why) => {
+            println!("fetch_whale::status-error {}", why);
+            return;
+        },
+        Ok(value) => {
+            status = value;
+        }
+    }
+
+    println!("Response: {:?}", status);
 
     let mut db = db.lock().unwrap();
 
