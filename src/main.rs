61d53d984705c86f19e495a1e6b8a0522e51f30b
650elx <bprokop02@gmail.com>
2021-03-18 22:12:05 +0100

    remove hyper, add regqwest

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-extern crate hyper;
-
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tokio::time::{sleep, Duration};
-use hyper::{Client};
 
 type Db = Arc<Mutex<HashMap<String, String>>>;
 
@@ -14,30 +11,32 @@ pub async fn main() {
     let db = Arc::new(Mutex::new(HashMap::new()));
 
     let process_db = db.clone();
-    let greed_db = db.clone();
-    let whale_db = db.clone();
-    let tickers_db = db.clone();
-
-    tokio::spawn(async move {
-        fetch_greed(greed_db).await;
-    });
-
-    tokio::spawn(async move {
-        fetch_whale(whale_db).await;
-    });
-
-    tokio::spawn(async move {
-        fetch_tickers(tickers_db).await;
-    });
 
     process(process_db).await;
 }
 
 async fn process(db: Db) {
     println!("-process-");
-    
+
     loop {
+        let greed_db = db.clone();
+        let whale_db = db.clone();
+        let tickers_db = db.clone();
+
         let db = db.lock().unwrap();
+
+
+        tokio::spawn(async move {
+            fetch_greed(greed_db).await;
+        });
+
+        tokio::spawn(async move {
+            fetch_whale(whale_db).await;
+        });
+
+        tokio::spawn(async move {
+            fetch_tickers(tickers_db).await;
+        });
 
         match db.get("hig") {
             None => println!("no value"),
@@ -60,7 +59,7 @@ async fn process(db: Db) {
             }
         }
 
-        sleep(Duration::from_millis(100)).await;
+        sleep(Duration::from_millis(4000)).await;
     }
 }
 
@@ -72,47 +71,25 @@ async fn fetch_greed(db: Db) {
     db.insert("hig".to_string(), "hi there greed".to_string()); 
 }
 
-async fn fetch_whale(db: Db) {
+async fn fetch_whale(db: Db) -> Result<(), Box<dyn std::error::Error>> {
     println!("-fetch_whale-");
 
-    let client = Client::new();
+    let resp = reqwest::get("https://api.whale-alert.io/v1/transactions?limit=100&min_value=500000&api_key=aZgs2HiCOeTkvMDGn1ltbvp1IysxRiQF")
+        .await?;
 
-    // let https = HttpsConnector::new();
-    // let client = Client::builder().build::<_, hyper::Body>(https);
-    
 
-    let uri = "http://api.whale-alert.io/v1/transactions?limit=100&start=1616085702&min_value=500000&api_key=aZgs2HiCOeTkvMDGn1ltbvp1IysxRiQF".parse();
-    let parsed_uri;
+        // .json::<HashMap<String, String>>()
+        // .await?;
 
-    match uri {
-        Err(why) => {
-            println!("fetch_whale::URI-ERROR {}", why);
-            return;
-        },
-        Ok(value) => {
-            parsed_uri = value;
-        }
-    }
-
-    let resp = client.get(parsed_uri).await;
-
-    let status;
-
-    match resp {
-        Err(why) => {
-            println!("fetch_whale::status-error {}", why);
-            return;
-        },
-        Ok(value) => {
-            status = value;
-        }
-    }
-
-    println!("Response: {:?}", status);
+    println!("Response: {:?}", resp);
+    let parsed = resp.json::<HashMap<String, String>>().await;
+    println!("{:?}", parsed);
 
     let mut db = db.lock().unwrap();
 
-    db.insert("hiw".to_string(), "hi there whale".to_string()); 
+    db.insert("hiw".to_string(), "hi there whale".to_string());
+
+    Ok(())
 }
 
 async fn fetch_tickers(db: Db) {
